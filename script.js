0caf5e1f744cc1726f5050be3227da2cf9471215
giginlila <gigeej009@gmail.com>
2023-09-23 14:41:05 +0930

    added prompts

--- script.js
+++ script.js
@@ -3,11 +3,31 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  //Define charcater sets for password criteria
+  var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+  var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()-_=+[]{}|;:'\"<>,.?/";
 
-  passwordText.value = password;
-}
+  // Initialize empty string for chars to include in the password
+  var characters = "";
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  //Prompt for password length
+  var passwordLnegth = parseInt(
+    prompt("Enter a password length (between 8 and 128 characters)")
+  );
+
+  //Validate password length
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLnegth > 128) {
+    alert("please enter a valid paswword length between 8 and 128 characters");
+    return "";
+  }
+
+  // confirm character types to include
+  var includeLowercase = confirm("include lowercase letters?");
+  var includeUppercase = confirm("include uppercase letters?");
+  var includeNumeric = confirm("include numeric characters");
+  var includeSpecial = confirm("include special characters?");
+
+  
+ 
